efb754279c6a7487ffbd370b5b0a83a0b7704ca8
calebabeck2020 <calebabeck@gmail.com>
2022-08-18 16:26:06 -0400

    started DishServlet, about to edit ServletContext to add Dish and Member to Servlet Container

--- src/main/java/com/revature/pokebowl/dish/DishServlet.java
+++ src/main/java/com/revature/pokebowl/dish/DishServlet.java
@@ -1,4 +1,44 @@
 package com.revature.pokebowl.dish;
 
-public class DishServlet {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.pokebowl.util.interfaces.Authable;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DishServlet extends HttpServlet implements Authable {
+
+    private final DishService dishService;
+    private final ObjectMapper objectMapper;
+    private final Logger logger = LogManager.getLogger();
+
+    public DishServlet(DishService dishService, ObjectMapper objectMapper) {
+        this.dishService = dishService;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
 }
