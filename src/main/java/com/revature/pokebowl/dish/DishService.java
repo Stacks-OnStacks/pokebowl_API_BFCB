cc24aa2db4a1050601c9f572c9668481f875c676
calebabeck2020 <calebabeck@gmail.com>
2022-08-18 16:50:11 -0400

    Injected DishDao Dependency into DishService to please the ServletContext

--- src/main/java/com/revature/pokebowl/dish/DishService.java
+++ src/main/java/com/revature/pokebowl/dish/DishService.java
@@ -1,5 +1,17 @@
 package com.revature.pokebowl.dish;
 
+import com.revature.pokebowl.dish.Dish;
+import com.revature.pokebowl.dish.DishDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class DishService {
 
+    private final DishDao dishDao;
+    private final Logger logger = LogManager.getLogger();
+
+    // CONSTRUCTOR
+    public DishService(DishDao dishDao){
+        this.dishDao = dishDao;
+    }
 }
