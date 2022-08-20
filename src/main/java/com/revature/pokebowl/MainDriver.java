e96238ca120616515347497425a6f95b7d5f9515
Byron <byronfedele@gmail.com>
2022-08-20 09:59:31 -0400

    Added some ASCII art to index and deleted some empty lines in MainDriver

--- src/main/java/com/revature/pokebowl/MainDriver.java
+++ src/main/java/com/revature/pokebowl/MainDriver.java
@@ -2,24 +2,18 @@ package com.revature.pokebowl;
 import com.revature.pokebowl.member.Member;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
-
 import com.revature.pokebowl.util.ServletContext;
 import com.revature.pokebowl.util.HibernateUtil;
 import org.hibernate.Transaction;
-
 import java.io.IOException;
 import java.sql.Date;
 import java.util.UUID;
-
 import static com.revature.pokebowl.util.HibernateUtil.getSession;
 
 public class MainDriver {
-
     public static void main(String[] args) {
-
         // Instantiate the ServletContext (run the program)
         ServletContext sc = new ServletContext();
         sc.run();
-
     }
 }
