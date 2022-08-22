d701a06f805d0b2346e09928f3b5af5a63dbee14
calebabeck2020 <calebabeck@gmail.com>
2022-08-21 22:44:17 -0400

    started ServiceTestSuites for PaymentService, OrderService, and OrderDetailsService

---
+++ src/test/java/com/revature/pokebowl/services/PaymentServiceSuite.java
@@ -0,0 +1,35 @@
+package com.revature.pokebowl.services;
+
+import com.revature.pokebowl.memberpayment.*;
+import org.junit.jupiter.api.*;
+import java.sql.Date;
+
+import static org.mockito.Mockito.*;
+
+public class PaymentServiceSuite {
+
+    PaymentService sut ;
+    PaymentDao mockPaymentDao;
+
+    @BeforeEach // this goes ahead and re-creates our sut every single test, so we are working with a fresh instance
+    public void testPrep(){
+        mockPaymentDao = mock(PaymentDao.class); // mocktail of the MemberDao Class not an actual instance, prevents massive memory
+        sut = new PaymentService(mockPaymentDao);
+    }
+
+    @Test
+    public void test_isPaymentValid_returnTrue_givenValidPayment(){
+        // 3 A's
+        // Arrange - setting up the information and resources you need for the test
+        // Act - performing the actual action on the unit your testing (invoking a method)
+        // Assert - evaluated if the actual result is as expected
+    }
+
+    @Test
+    public void test_isPaymentValid_returnFalse_givenInvalidPayment(){
+        // 3 A's
+        // Arrange - setting up the information and resources you need for the test
+        // Act - performing the actual action on the unit your testing (invoking a method)
+        // Assert - evaluated if the actual result is as expected
+    }
+}
