ee547eab714b0c4397c88e2d0651e8834706e3cd
Byron <byronfedele@gmail.com>
2022-08-24 17:23:31 -0400

    Still working on OrderDetails, merging now to avoid merge conflicts

--- src/main/java/com/revature/pokebowl/orderdetails/OrderDetailsService.java
+++ src/main/java/com/revature/pokebowl/orderdetails/OrderDetailsService.java
@@ -1,8 +1,8 @@
 package com.revature.pokebowl.orderdetails;
 import com.revature.pokebowl.dish.DishService;
-import com.revature.pokebowl.member.MemberService;
 import com.revature.pokebowl.dish.DishDao;
-import com.revature.pokebowl.member.MemberService;
+import com.revature.pokebowl.memberpayment.Payment;
+import com.revature.pokebowl.memberpayment.dto.responses.PaymentResponse;
 import com.revature.pokebowl.order.OrderService;
 import com.revature.pokebowl.orderdetails.OrderDetails;
 import com.revature.pokebowl.orderdetails.dto.requests.CreateOrderDetailsRequest;
@@ -14,10 +14,11 @@ import com.revature.pokebowl.util.exceptions.ResourcePersistanceException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class OrderDetailsService {
-    private final MemberService memberService;
     private final DishService dishService;
     private final OrderService orderService;
 
@@ -25,8 +26,9 @@ public class OrderDetailsService {
     private final Logger logger = LogManager.getLogger();
 
     // CONSTRUCTOR
-    public OrderDetailsService(DishService dishService, OrderDetailsDao orderDetailsDao){
-        this.memberService = memberService;
+    public OrderDetailsService(DishService dishService, OrderService orderService, OrderDetailsDao orderDetailsDao){
+        this.dishService = dishService;
+        this.orderService = orderService;
         this.orderDetailsDao = orderDetailsDao;
 
     }
@@ -38,5 +40,23 @@ public class OrderDetailsService {
         orderDetailsDao.create(newOrderDetails);
         return new OrderDetailsResponse(newOrderDetails);
     }
-
+    public List<OrderDetailsResponse> readAll(){
+        // Streams are a form of functional programming this is form a declarative programming
+        List<OrderDetailsResponse> orderDetails = orderDetailsDao.findAll()
+                .stream()//this reads through each value inside of the collection (aka our List)
+                //.map(member -> new MemberResponse(member))
+                // this is leveraging (::) which is known as the method reference operator, it's taking the method from MemberReponse and applying to all objects in the stream
+                .map(OrderDetailsResponse::new)
+                .collect(Collectors.toList());
+        return orderDetails;
+    }
+    public OrderDetailsResponse findById(String orderDetailsId){
+        OrderDetails orderDetails = orderDetailsDao.findById(orderDetailsId);
+        if (orderDetails == null) return null;
+        OrderDetailsResponse paymentResponse = new OrderDetailsResponse(orderDetails);
+        return paymentResponse;
+    }
+    public boolean remove(String orderDetailsId){
+        return orderDetailsDao.delete(orderDetailsId);
+    }
 }
